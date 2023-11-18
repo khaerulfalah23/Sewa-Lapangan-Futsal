f4d892002fd7f0b0642a6ce178dda565466d8b7e
luthfigilang <muhammadluthfigilangpratama16@gmail.com>
2023-11-18 10:16:58 +0700

    Menyelesaikan Component Navbar

---
+++ application/controllers/User.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('templates/header');
+		$this->load->view('templates/navbar');
+		$this->load->view('templates/footer');
+	}
+}
