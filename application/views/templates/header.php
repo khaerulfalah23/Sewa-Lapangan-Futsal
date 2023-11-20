84733ecb6944d11ae7827f2ea82ee1886828942c
ririnnurtriani <ririnnurtrn@gmail.com>
2023-11-20 20:59:51 +0700

    Menyelesaikan halaman cara penyewaan

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,8 +8,23 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <!-- Bootstrap CSS -->
+    <!-- MY CSS -->
     <link rel="stylesheet" href="<?= base_url('assets/user/'); ?>style.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url() ?>assets/user/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/user/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/user/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/user/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/user/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url() ?>assets/user/css/style.css" rel="stylesheet">
+
+    
     
     <title>Sewa Lapangan Futsal</title>
   </head>
---
+++ application/views/user/cara_penyewaan.php
@@ -0,0 +1,63 @@
+<section id="what-we-do" class="what-we-do">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>CARA MELAKUKAN PENYEWAAN</h2>
+          <p style="text-transform: Capitalize;">Penyewaan dilakukan secara online melalui form penyewaan yang telah disediakan di website</p>
+        </div>
+
+        <div class="row mb-5">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <h2 class="icon">1</h2>
+              <h4><a href="">REGISTRASI</a></h4>
+              <p>Langkah pertama untuk melakukan pemesanan lapangan futsal yaitu harus melakukan registrasi di form registrasi.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <h2 class="icon">2</h2>
+              <h4><a href="">LOGIN</a></h4>
+              <p>Setelah registrasi berhasil silahkan login menggunakan username dan password yang telah di registrasi.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <h2 class="icon">3</h2>
+              <h4><a href="">FORM PENYEWAAN</a></h4>
+              <p>Silahkan isi form penyewaan sesuai dengan apa yang di inginkan.</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <h2 class="icon">4</h2>
+              <h4><a href="">BUKTI PEMESANAN</a></h4>
+              <p>Print atau screenshot bukti pemesanan dan simpan bukti pemesanan.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <h2 class="icon">5</h2>
+              <h4><a href="">PEMBAYARAN</a></h4>
+              <p>Silahkan bayar sesuai dengan jumlah yang tertera di bukti pemesanan dan simpan bukti pembayaran.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <h2 class="icon">6</h2>
+              <h4><a href="">VERIFIKASI</a></h4>
+              <p>Berikan bukti pembayaran ke admin untuk dilakukan verifikasi.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End What We Do Section -->
