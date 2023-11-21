1e5cd68b2b738db4bf0d73bb520ca0436b630503
luthfigilang <muhammadluthfigilangpratama16@gmail.com>
2023-11-21 13:58:23 +0700

    Memperbaiki Component Navbar

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,6 +5,10 @@ class User extends CI_Controller {
 
 	public function index()
 	{
+		$email = $this->session->userdata('email');
+        $data = [
+            'user' => $this->db->get_where('user',['email' => $email])->row_array()
+        ];
 		$this->load->view('templates/header');
 		$this->load->view('templates/navbar');
 		$this->load->view('user/tentangkami');
