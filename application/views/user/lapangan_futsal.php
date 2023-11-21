56b60b60bfa0afd7392d6433b03bebaeee121739
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-11-21 07:29:21 +0700

    Menyelesaikan Component Lapangan Futsat(lightbox)

---
+++ application/views/user/lapangan_futsal.php
@@ -0,0 +1,108 @@
+<section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="section-title" style="text-transform: Capitalize; margin-bottom: 0;">
+          <h2>LAPANGAN FUTSAL</h2>
+          <p style="font-size: 14px;">Kami Mempunyai dua Jenis Lapangan Futsal lapangan Matras Dan <br> Lapangan Sintetis yang siap untuk di gunakan</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">Lapangan Matras</li>
+              <li data-filter=".filter-web">Lapangan Sintetis</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/matras-1.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/matras-1.jpg" data-gallery="portfolioGallery" class="link-preview portfolio-lightbox" title="Lapangan Matras"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_matras'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="">Lapangan Matras</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/sintetis-1.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/sintetis-1.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Lapangan Sintetis"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_sintetis'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Lapangan Sintetis</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/matras-2.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/matras-2.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Lapangan Matras"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_matras'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="">Lapangan Matras</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.1s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/sintetis-2.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/sintetis-2.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Lapangan Sintetis"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_sintetis'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Lapangan Sintetis</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/matras-3.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/matras-3.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Lapangan Matras"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_matras'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="">Lapangan Matras</a></h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp" data-wow-delay="0.2s">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="<?= base_url('assets/user/'); ?>img/sintetis-3.jpg" class="img-fluid" alt="">
+                <a href="<?= base_url('assets/user/'); ?>img/sintetis-3.jpg" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="Lapangan Sintetis"><i class="bx bx-plus"></i></a>
+                <a href="<?= base_url('user/lapangan_sintetis'); ?>" class="link-details" title="More Details"><i class="bx bx-link"></i></a>
+              </figure>
+
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Lapangan Sintetis</a></h4>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
