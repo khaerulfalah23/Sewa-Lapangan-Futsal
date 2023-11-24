c306d8d84d474efe34fa8ca79e1b69f52d03d196
ririnnurtriani <ririnnurtrn@gmail.com>
2023-11-24 21:15:18 +0700

    Menyelesaikan Halaman Lapangan Sintetis

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,4 +18,12 @@ class User extends CI_Controller {
 		$this->load->view('user/cara_penyewaan');
 		$this->load->view('templates/footer');
 	}
+
+	public function lapangan_sintetis()
+	{
+        $data['usersesion'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+		$this->load->view('templates/header',$data);
+		$this->load->view('user/lapangan_sintetis');
+		$this->load->view('templates/footer');
+	}
 }
